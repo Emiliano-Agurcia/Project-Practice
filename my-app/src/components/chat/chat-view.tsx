a75267bf05daf0a71ea083b9ef974db4c8fce627
Emile <emilianoagurcia@gmail.com>
2025-10-31 13:00:19 -0600

    chat header

--- my-app/src/components/chat/chat-view.tsx
+++ my-app/src/components/chat/chat-view.tsx
@@ -42,8 +42,8 @@ export default function ChatView({ initialMessages }: ChatViewProps) {
 
   return (
     <div className="flex flex-col w-full h-screen max-w-6xl mx-auto shadow-2xl bg-chat-bg">
-       <header className="p-4 border-b shadow-sm border-border">
-        <h1 className="text-2xl font-bold text-foreground font-headline">ChatBot</h1>
+       <header className="p-4 border-b shadow-sm border-border bg-muted">
+        <h1 className="text-2xl text-foreground font-headline font-primary">ChatBot</h1>
         
         <p className="flex flex-row gap-2 w-max text-md text-primary"><Circle className="self-center w-3 h-3 fill-test sm:fill-test1 lg:fill-test2" />Online</p>
        </header>
